8d6c59f04553f18a24a3a886c4eb9ae654629db3
Marsild <marsild.spahiu@studio.unibo.it>
2020-11-30 12:12:42 +0100

    Fine esercizio 2

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,95 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class ConcurrentGUI extends JFrame {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8630968055862320453L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        this.display.setText("0");
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        final ReactiveGui2 es2 = new ReactiveGui2();
+        es2.start();
+        stop.addActionListener(e -> es2.stopCounting());
+        up.addActionListener(e -> es2.setUpFlag());
+        down.addActionListener(e -> es2.setDownFlag());
+    }
+    private class ReactiveGui2 extends Thread {
+        private volatile boolean stop;
+        private volatile boolean direction = true; //true incrementa, false decrementa
+        private volatile int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    /*
+                     * All the operations on the GUI must be performed by the
+                     * Event-Dispatch Thread (EDT)!
+                     */
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(Integer.toString(this.counter)));
+                    if (direction) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    /*
+                     * This is just a stack trace print, in a real program there
+                     * should be some logging and decent error reporting
+                     */
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+            ConcurrentGUI.this.up.setEnabled(false);
+            ConcurrentGUI.this.down.setEnabled(false);
+        }
+        /**
+         * External command to increment.
+         */
+        public void setUpFlag() {
+            this.direction = true;
+        }
+        /**
+         * External command to decrement.
+         */
+        public void setDownFlag() {
+            this.direction = false;
+        }
+    }
+}
